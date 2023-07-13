395956cef9cf0354cbcef841b6574d2c03131970
Виталий <vmonovv@gmail.com>
2023-07-13 19:14:04 +0300

    123

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,32 +46,32 @@ export default defineNuxtConfig({
         gtag('config', 'G-5ZMFKJYM1N');`,
         },
 
-      //   {
-      //     innerHTML: `
-      //     <!-- Yandex.Metrika counter -->
-      //     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      //     m[i].l=1*new Date();
-      //     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-      //     k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      //     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        {
+          innerHTML: `
+          <!-- Yandex.Metrika counter -->
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
        
-      //     ym(91305537, "init", {
-      //          clickmap:true,
-      //          trackLinks:true,
-      //          accurateTrackBounce:true,
-      //          webvisor:true
-      //     });
-      //    `,
-      //   },
-      // ],
-      // noscript: [
-      //   {
-      //     innerHTML: `
-      // <!-- Yandex.Metrika counter -->
-      // <div><img src="https://mc.yandex.ru/watch/91305537" style="position:absolute; left:-9999px;" alt="" /></div>
-      // <!-- Yandex.Metrika counter -->
-      //  `,
-      //   },
+          ym(91305537, "init", {
+               clickmap:true,
+               trackLinks:true,
+               accurateTrackBounce:true,
+               webvisor:true
+          });
+         `,
+        },
+      ],
+      noscript: [
+        {
+          innerHTML: `
+      <!-- Yandex.Metrika counter -->
+      <div><img src="https://mc.yandex.ru/watch/91305537" style="position:absolute; left:-9999px;" alt="" /></div>
+      <!-- Yandex.Metrika counter -->
+       `,
+        },
       ],
 
       meta: [
@@ -135,7 +135,7 @@ export default defineNuxtConfig({
   },
 
   css: ["@/assets/css/style.css"],
-  modules: ["@nuxt/image", "@nuxt/content", "@nuxtjs/i18n"],
+  modules: ["@nuxt/image", "@nuxt/content", "@nuxtjs/i18n", "nuxt-compress"],
   image: {
     provider: 'netlify',
     format: ['webp'],
