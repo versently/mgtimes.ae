7e82109286e1b53efee74302ecede21910a81ef6
Виталий <vmonovv@gmail.com>
2023-07-14 19:31:48 +0300

    img update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,46 +47,45 @@ export default defineNuxtConfig({
           }),
         },
 
-      //   {
-      //     defer: true,
-      //     src: "https://www.googletagmanager.com/gtag/js?id=G-5ZMFKJYM1N",
-      //   },
+        //   {
+        //     defer: true,
+        //     src: "https://www.googletagmanager.com/gtag/js?id=G-5ZMFKJYM1N",
+        //   },
 
-      //   {
-      //     innerHTML: `window.dataLayer = window.dataLayer || [];
-      //   function gtag(){dataLayer.push(arguments);}
-      //   gtag('js', new Date());
-      
-      //   gtag('config', 'G-5ZMFKJYM1N');`,
-      //   },
+        //   {
+        //     innerHTML: `window.dataLayer = window.dataLayer || [];
+        //   function gtag(){dataLayer.push(arguments);}
+        //   gtag('js', new Date());
 
-      //   {
-      //     innerHTML: `
-      //     <!-- Yandex.Metrika counter -->
-      //     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      //     m[i].l=1*new Date();
-      //     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-      //     k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      //     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-       
-      //     ym(91305537, "init", {
-      //          clickmap:true,
-      //          trackLinks:true,
-      //          accurateTrackBounce:true,
-      //          webvisor:true
-      //     });
-      //    `,
-      //   },
-      // ],
-      // noscript: [
-      //   {
-      //     innerHTML: `
-      // <!-- Yandex.Metrika counter -->
-      // <div><img src="https://mc.yandex.ru/watch/91305537" style="position:absolute; left:-9999px;" alt="" /></div>
-      // <!-- Yandex.Metrika counter -->
-      //  `,
-      //   },
-   
+        //   gtag('config', 'G-5ZMFKJYM1N');`,
+        //   },
+
+        //   {
+        //     innerHTML: `
+        //     <!-- Yandex.Metrika counter -->
+        //     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        //     m[i].l=1*new Date();
+        //     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        //     k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        //     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        //     ym(91305537, "init", {
+        //          clickmap:true,
+        //          trackLinks:true,
+        //          accurateTrackBounce:true,
+        //          webvisor:true
+        //     });
+        //    `,
+        //   },
+        // ],
+        // noscript: [
+        //   {
+        //     innerHTML: `
+        // <!-- Yandex.Metrika counter -->
+        // <div><img src="https://mc.yandex.ru/watch/91305537" style="position:absolute; left:-9999px;" alt="" /></div>
+        // <!-- Yandex.Metrika counter -->
+        //  `,
+        //   },
       ],
 
       meta: [
@@ -118,6 +117,12 @@ export default defineNuxtConfig({
         { property: "og:image:height", content: "600" },
       ],
       link: [
+        {
+          rel: "preload",
+          as: "image",
+          href: "https://mgtimes.ae/_nuxt/bg-image.2fb040e1.jpg",
+        },
+
         {
           rel: "stylesheet",
           href: "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css",
