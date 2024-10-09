471e7dd6c8428b8a3417c3d03b35d7fce6737ccc
versently <versently@gmail.com>
2024-10-09 19:16:32 +0300

    delete .nuxt directory

--- .nuxt/prerender/chunks/app/_nuxt/index-styles.a03e2b4a.mjs
+++
@@ -1,6 +0,0 @@
-const index_vue_vue_type_style_index_0_lang = ".service-item img{height:100%;max-height:272px;-o-object-fit:cover;object-fit:cover}";
-
-const indexStyles_a03e2b4a = [index_vue_vue_type_style_index_0_lang, index_vue_vue_type_style_index_0_lang];
-
-export { indexStyles_a03e2b4a as default };
-//# sourceMappingURL=index-styles.a03e2b4a.mjs.map
