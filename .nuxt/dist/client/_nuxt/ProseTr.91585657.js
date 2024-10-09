4f512003832514f6c410122cd3aba135fb857fc1
versently <versently@gmail.com>
2024-10-09 19:17:01 +0300

    delete .nuxt

--- .nuxt/dist/client/_nuxt/ProseTr.91585657.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o,c as t,N as s}from"./entry.573f58c0.js";const c={};function n(e,a){return o(),t("tr",null,[s(e.$slots,"default")])}const _=r(c,[["render",n]]);export{_ as default};
--- .nuxt/dist/server/_nuxt/entry-styles.daa4a62b.mjs
+++
@@ -1,2 +0,0 @@
-import style_0 from './entry-styles-1.mjs-e07e503f.js';
-export default [style_0]
