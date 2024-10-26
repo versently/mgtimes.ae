dd54d6b0d90f145c8a1dfccd451414e2a4aa4a9e
versently <versently@gmail.com>
2024-10-26 10:30:34 +0300

    fix

--- pages/models/[...slug].vue
+++ pages/models/[...slug].vue
@@ -35,10 +35,12 @@ useHead({
     " " +
     data.value.models.title +
     ". " +
+    t("Height") +
+    " " +
+    data.value.models.height + ". " +
     t("Age") +
     " " +
-    data.value.models.age +
-    " " +
+    data.value.models.age + " " +
     t("years old - MGtimes VIP escort in Dubai"),
 
   meta: [
@@ -78,16 +80,8 @@ const images = data.value.models.images;
 <template>
   <div class="model">
     <div class="model__container">
-      <div
-        class="casting__way-block bread-crumblist"
-        itemscope=""
-        itemtype="https://schema.org/BreadcrumbList"
-      >
-        <li
-          itemprop="itemListElement"
-          itemscope=""
-          itemtype="https://schema.org/ListItem"
-        >
+      <div class="casting__way-block bread-crumblist" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+        <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
           <a itemprop="item" :href="localePath('index')">
             <span itemprop="name">{{ t("Home page") }}</span>
             <meta itemprop="position" content="1" />
@@ -95,39 +89,24 @@ const images = data.value.models.images;
         </li>
         /
 
-        <li
-          itemprop="itemListElement"
-          itemscope=""
-          itemtype="https://schema.org/ListItem"
-        >
+        <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
           <a :href="localePath('models')" itemprop="item">
             <span itemprop="name">{{ t("Models") }}</span>
             <meta itemprop="position" content="2" />
           </a>
         </li>
         /
-        <li
-          itemprop="itemListElement"
-          itemscope=""
-          itemtype="https://schema.org/ListItem"
-        >
+        <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
           <span itemprop="name">{{ data.models.title }}</span>
           <meta itemprop="position" content="3" />
         </li>
       </div>
       <div class="model__content">
         <div class="model__images">
-          <nuxt-img
-        
-            :src="`/assets/img/models/${data.models.folder}/${data.models.mainImage}`"
-            :alt="`${data.models.folder}`"
-          />
-          <nuxt-img
-            v-for="(image, index) in images"
-            :key="index"
-            :src="`/assets/img/models/${data.models.folder}/${image}`"
-            :alt="`${data.models.folder}`"
-          />
+          <nuxt-img :src="`/assets/img/models/${data.models.folder}/${data.models.mainImage}`"
+            :alt="`${data.models.folder}`" />
+          <nuxt-img v-for="(image, index) in images" :key="index"
+            :src="`/assets/img/models/${data.models.folder}/${image}`" :alt="`${data.models.folder}`" />
         </div>
         <div class="model__info">
           <h1 class="model__info-name">{{ data.models.title }}</h1>
@@ -158,15 +137,8 @@ const images = data.value.models.images;
         </div>
         <div class="model__swiper-img">
           <div class="model__swiper-wrapper-img">
-            <div
-              class="model__swiper-slide-img"
-              v-for="(image, index) in images"
-              :key="index"
-            >
-              <nuxt-img
-                :src="`/assets/img/models/${data.models.folder}/${image}`"
-                alt=""
-              />
+            <div class="model__swiper-slide-img" v-for="(image, index) in images" :key="index">
+              <nuxt-img :src="`/assets/img/models/${data.models.folder}/${image}`" alt="" />
             </div>
           </div>
           <div class="model__swiper-scrollbar-img"></div>
@@ -188,42 +160,33 @@ const images = data.value.models.images;
             </p>
             <div class="models-section__swiper">
               <!-- Additional required wrapper -->
-              <ContentList
-                :path="localePath('/models')"
-                :query="{
-                  only: [
-                    'title',
-                    'description',
-                    'tags',
-                    '_path',
-                    'img',
-                    'folder',
-                    'mainImage',
-                    'height',
-                    'weight',
-                    'age',
-                  ],
-                  where: {
-                    tags: {
-                      $contains: filter,
-                    },
+              <ContentList :path="localePath('/models')" :query="{
+                only: [
+                  'title',
+                  'description',
+                  'tags',
+                  '_path',
+                  'img',
+                  'folder',
+                  'mainImage',
+                  'height',
+                  'weight',
+                  'age',
+                ],
+                where: {
+                  tags: {
+                    $contains: filter,
                   },
-                  $sensitivity: 'base',
-                }"
-              >
+                },
+                $sensitivity: 'base',
+              }">
                 <!-- Default list slot -->
                 <template v-slot="{ list }">
                   <div class="models-section__swiper-wrapper">
                     <!-- Slides -->
-                    <div
-                      class="models-section__swiper-slide"
-                      v-for="models in list"
-                      :key="models._path"
-                    >
-                      <nuxt-img
-                        :src="`/assets/img/models/${models.folder}/${models.mainImage}`"
-                        :alt="`${models.folder}`"
-                      />
+                    <div class="models-section__swiper-slide" v-for="models in list" :key="models._path">
+                      <nuxt-img :src="`/assets/img/models/${models.folder}/${models.mainImage}`"
+                        :alt="`${models.folder}`" />
                       <div class="models-section__slide-description">
                         <div class="models-section__slide-name">
                           {{ models.title }}
@@ -264,14 +227,12 @@ const images = data.value.models.images;
 
 <style lang="scss" scoped></style>
 
-<i18n lang="json">
-{
+<i18n lang="json">{
   "en": {
     "Top Model": "Top Model",
     "years old - MGtimes VIP escort in Dubai": "years old - MGtimes VIP escort in Dubai",
     "My name is": "My name is",
     "Want to know my phone number? Order VIP escort services in Dubai in the best escort agency in Dubai MGtimes": "Want to know my phone number? Order VIP escort services in Dubai in the best escort agency in Dubai MGtimes",
-
     "Models": "Models",
     "Home page": "Home page",
     "Height": "Height",
@@ -288,7 +249,6 @@ const images = data.value.models.images;
     "years old - MGtimes VIP escort in Dubai": "лет - VIP эскорт MGtimes в Дубае",
     "My name is": "Меня зовут",
     "Want to know my phone number? Order VIP escort services in Dubai in the best escort agency in Dubai MGtimes": "Хотите Узнать мой телефон? Закажи VIP эскорт услуги в лучшем эскорт-агентстве Дубая MGtimes ",
-
     "Models": "Модели",
     "Home page": "Главная страница",
     "Height": "Рост",
@@ -300,5 +260,4 @@ const images = data.value.models.images;
     "More models": "Ещё модели",
     "Below you can see profiles of beautiful girl to convey their beauty, femininity and elegance.": "Ниже вы можете увидеть больше красивых девушек"
   }
-}
-</i18n>
+}</i18n>
