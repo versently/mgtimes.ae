153ad0c11f6a2e55502f1a935ef6a63eb7e3b97f
Виталий <versently@gmail.com>
2025-02-17 19:02:21 +0300

    gitignore

---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('/Users/vitaly/Sites/hostiman_artur/mgtimes.ae/mgtimes.ae/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
+  const parseContent: typeof import('/Users/vitaly/Sites/hostiman_artur/mgtimes.ae/mgtimes.ae/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
